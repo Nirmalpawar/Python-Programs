356852ba3003f983b58cd61c19129a4d0fdcc680
Nirmal Pawar <141116277+Nirmalpawar@users.noreply.github.com>
2024-03-15 12:33:57 +0530

    Add files via upload

---
+++ Armstrong.py
@@ -0,0 +1,21 @@
+n=int(input("Enter Number to check:"))
+i=n
+count=0
+while(i>0):
+    count=count+1
+    i=i//10
+sum=0
+i=n
+while(i>0):
+    digit=i%10
+    x=1
+    pro=1
+    while(x<=count):
+        pro=pro*digit
+        x=x+1
+    sum= sum+pro
+    i=i//10
+    if (sum==n):
+            print("Armstrong Number")
+    else:
+            print("Not Armstrang Number")
